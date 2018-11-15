fe81a8db55eba6af125abadb733dd750d8ab4b41
olejnicz5u <olejnicz5u@etu.univ-lorraine.fr>
2018-11-15 12:14:32 +0100

    Ajout de la recherche de recettes par sous chaine d'ingrédients

--- page_principale.php
+++ page_principale.php
@@ -8,24 +8,24 @@
         <title>Projet Boissons</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link rel="stylesheet" type="text/css" href="page_principale.css">
-		<script language="JavaScript">
+	
+        <script language="JavaScript">
 
             function hideMenuInit(str) {
                 document.getElementById(str).style.display='none';
             }
 
-			function showMenuItem(str) {
-
-			    var doc = document.getElementById(str);
+            function showMenuItem(str) {
+                var doc = document.getElementById(str);
                 if ($(doc).is (':visible')) {
                     doc.style.display='none';
                 }
                 else {
                     doc.style.display='block';
                 }
-			}
+            }
 
-			function showRecipes(elem) {
+            function showRecipes(elem) {
                 if (elem.length === 0) {
                     document.getElementById("recipes").innerHTML = "";
                 }
@@ -41,63 +41,84 @@
                 xhttp.send();
             }
 			
-		</script>
+	</script>
 	</head>
-	<body >
+        
+	<body>
+        <!--Recherche la saisie de l'utilisateur dans les données-->
+        <input id="search" type="search" name="liste_recherche" placeholder="Rechercher..."
+               list="hierarchies" />
+            <!--Menu déroulant sous la barre de recherche-->
+            <datalist id="hierarchies">
+                <?php
+                    $i = 0;
+                    foreach($Hierarchie as $element){
+                        // Affichage de chaque élément contenant la chaîne saisie
+                        echo "<option>".array_keys($Hierarchie)[$i]."</option>";
+                        $i++;
+                    }
+                ?>
+            </datalist>
+        <!--Bouton pour valider la recherche-->
+        <input id="submit" type="submit" name="bouton_recherche" value="Rechercher" 
+            onclick="showRecipes(search.value)"/>
+         
+         
         <dl id="menu">
             <p id='menu_pres'>Recherche par catégories</p>
-        <?php
-            $i = 0;
-            $GLOBALS['j'] = 0;
-            foreach ($Hierarchie as $element) {
-               if (!array_key_exists('super-categorie', $element)) {
-                   echo "<div class='elem_init'>";
-                   echo "<dt onClick='showMenuItem(\"menu".$GLOBALS['j']."\")'> <a href='#'>".array_keys($Hierarchie)[$i]."</a></dt>";
-                   echo "</div>";
-                   echo "<dl id='menu".$GLOBALS['j']."'>";
-                   echo "<script type='text/JavaScript'> hideMenuInit(\"menu".$GLOBALS['j']."\"); </script>";
-                   $GLOBALS['j']++;
-                   displayElementHierarchy($Recettes, $Hierarchie, $element, 1);
-                   echo "</dl>";
-               }
-               $i++;
-            }
+            <?php
+                $i = 0;
+                $GLOBALS['j'] = 0;
+                foreach ($Hierarchie as $element) {
+                   if (!array_key_exists('super-categorie', $element)) {
+                       echo "<div class='elem_init'>";
+                       echo "<dt onClick='showMenuItem(\"menu".$GLOBALS['j']."\")'> <a href='#'>".array_keys($Hierarchie)[$i]."</a></dt>";
+                       echo "</div>";
+                       echo "<dl id='menu".$GLOBALS['j']."'>";
+                       echo "<script type='text/JavaScript'> hideMenuInit(\"menu".$GLOBALS['j']."\"); </script>";
+                       $GLOBALS['j']++;
+                       displayElementHierarchy($Recettes, $Hierarchie, $element, 1);
+                       echo "</dl>";
+                   }
+                   $i++;
+                }
 
-            function displayElementHierarchy($Recettes, $Hierarchie, $elemSearch, $level) {
-                foreach ($elemSearch['sous-categorie'] as $sub_element) {
-                    if (array_key_exists('sous-categorie', $Hierarchie[$sub_element])) {
-                        echo "<div class='elem".$level % 3 ."'>";
-                        echo "<dt onClick='showMenuItem(\"menu".$GLOBALS['j']."\")'>";
-                        for ($k = 0 ; $k < $level ; $k++) {
-                            echo "&nbsp;&nbsp;&nbsp;";
+                function displayElementHierarchy($Recettes, $Hierarchie, $elemSearch, $level) {
+                    foreach ($elemSearch['sous-categorie'] as $sub_element) {
+                        if (array_key_exists('sous-categorie', $Hierarchie[$sub_element])) {
+                            echo "<div class='elem".$level % 3 ."'>";
+                            echo "<dt onClick='showMenuItem(\"menu".$GLOBALS['j']."\")'>";
+                            for ($k = 0 ; $k < $level ; $k++) {
+                                echo "&nbsp;&nbsp;&nbsp;";
+                            }
+                            echo "<a href='#'>".$sub_element."</a></dt>";
+                            echo "</div>";
+                            echo "<dl id='menu".$GLOBALS['j']."'>";
+                            echo "<script type='text/JavaScript'> hideMenuInit(\"menu".$GLOBALS['j']."\"); </script>";
+                            $GLOBALS['j']++;
+                            $level_temp = $level + 1;
+                            displayElementHierarchy($Recettes, $Hierarchie, $Hierarchie[$sub_element], $level_temp);
+                            echo "</dl>";
                         }
-                        echo "<a href='#'>".$sub_element."</a></dt>";
-                        echo "</div>";
-                        echo "<dl id='menu".$GLOBALS['j']."'>";
-                        echo "<script type='text/JavaScript'> hideMenuInit(\"menu".$GLOBALS['j']."\"); </script>";
-                        $GLOBALS['j']++;
-                        $level_temp = $level + 1;
-                        displayElementHierarchy($Recettes, $Hierarchie, $Hierarchie[$sub_element], $level_temp);
-                        echo "</dl>";
-                    }
-                    else {
-                        echo "<div class='elem".$level % 3 ."'>";
-                        echo "<dt onClick='showRecipes(\"".$sub_element."\")'>";
-                        for ($k = 0 ; $k < $level ; $k++) {
-                            echo "&nbsp;&nbsp;&nbsp;";
+                        else {
+                            echo "<div class='elem".$level % 3 ."'>";
+                            echo "<dt onClick='showRecipes(\"".$sub_element."\")'>";
+                            for ($k = 0 ; $k < $level ; $k++) {
+                                echo "&nbsp;&nbsp;&nbsp;";
+                            }
+                            echo "<a href='#'>".$sub_element."</a></dt>";
+                            echo "</div>";
+                            echo "<dl id='menu".$GLOBALS['j']."'>";
+                            echo "<script type='text/JavaScript'> hideMenuInit(\"menu".$GLOBALS['j']."\"); </script>";
+                            $GLOBALS['j']++;
+                            echo "</dl>";
                         }
-                        echo "<a href='#'>".$sub_element."</a></dt>";
-                        echo "</div>";
-                        echo "<dl id='menu".$GLOBALS['j']."'>";
-                        echo "<script type='text/JavaScript'> hideMenuInit(\"menu".$GLOBALS['j']."\"); </script>";
-                        $GLOBALS['j']++;
-                        echo "</dl>";
                     }
                 }
-            }
 
-        ?>
+            ?>
         </dl>
+         
         <div id='recipes'>
             <p> Les recettes seront affichées ici.</p>
         </div>
